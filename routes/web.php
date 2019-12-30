90460fb0358807f6dab04f16d1e552c7a136ce89
deltacastro <abel.castro9111@gmail.com>
2019-12-30 21:54:38 +0000

    Se hace un redirect si el usuario ya se logueo

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::user() != null) {
+        return redirect()->route('home');
+    }
+    return view('auth.login');
 });
 
 Auth::routes();
