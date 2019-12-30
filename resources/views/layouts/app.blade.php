df80029912cd0a44c243f9e6e416a7d11931206e
deltacastro <abel.castro9111@gmail.com>
2019-12-30 23:18:35 +0000

    Se modifica se agregan yields style y javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     {{-- <link href="{{ asset('css/bootstrap.css.map') }}"> --}}
+
+    @yield('style')
 </head>
 <body>
     <div id="app">
@@ -31,5 +33,7 @@
             @yield('content')
         </main>
     </div>
+    @yield('javascript')
+    @stack('javascript')
 </body>
 </html>
