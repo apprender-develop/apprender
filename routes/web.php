e63459873d1c13ea9485da19bbb20c8ed64e85c1
deltacastro <abel.castro9111@gmail.com>
2019-12-30 23:18:52 +0000

    Se agrega ruta para dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'dashboard'], function () {
+    Route::get('graficas', 'DashboardController@index');
+});
