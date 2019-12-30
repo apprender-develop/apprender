f77afc91d1e3687b20b1070f15af406a3d7a8e70
deltacastro <abel.castro9111@gmail.com>
2019-12-30 23:38:34 +0000

    vistas para sandbox

---
+++ resources/views/sandbox/jona/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+{{-- Aqui pones los estilos --}}
+@section('style')
+    <style>
+
+    </style>
+@endsection
+
+
+@section('content')
+
+@endsection
+
+@section('javascript')
+    <script>
+
+    </script>
+@endsection
