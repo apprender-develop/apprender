4b2b3029e3971cd231abd7afb98220270e21f25f
deltacastro <abel.castro9111@gmail.com>
2019-12-31 18:46:55 +0000

    Se agregan yields de header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    @yield('header-layout')
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/jquery-3.4.1.min.map') }}"></script> --}}
     <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
@@ -32,6 +32,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @yield('content2')
     </div>
     @yield('javascript')
     @stack('javascript')
