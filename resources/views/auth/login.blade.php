de86c583679593a7ab74d879138cce9e2d271623
deltacastro <abel.castro9111@gmail.com>
2019-12-31 19:15:34 +0000

    se actualiza el login con el proyecto jamarrom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,85 @@
-@extends('layouts.app')
+@extends('layouts.app2')
+
+@section('header-layout')
+    <title>Login - Apprender</title>
+	<link rel="shortcut icon" href="favicon.png" />
+@endsection
+
+@section('style')
+    <link href="{{'/css/jamarrom/reset.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/fonts.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/utilidades.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/style.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/animations.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/responsive.css'}}" rel="stylesheet" type="text/css">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="Autentificacion-Ajuste u-imagenFondoCover">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <article class="Login" id="login">
+        <h1 class="Login-Logo slideDown"><span>Apprender</span></h1>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <form name="form-login" id="form-login" class="Login-Form u-redondeado--1 u-desaparecer" action="{{ route('login') }}" method="POST">
+            @csrf
+            <p class="Login-Titulo u-textLeft">
+                Iniciar sesión
+            </p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <input type="email" name="email" id="txtUsuario"
+                class="Login-Usuario text u-box-sizing required  u-redondeado--05"
+                placeholder="INGRESE E-MAIL DE USUARIO"
+                onfocus="if(this.value == 'INGRESE E-MAIL DE USUARIO') { this.value = ''; }"
+                value="<?php if(isset($_POST['btnEntrar'])) { if(!empty($msj)) { echo $usuario; } } ?>"
+                required autocomplete="email" autofocus>
+            @error('email')
+                    <strong>{{ $message }}</strong>
+            @enderror
+            <input type="password" name="password" id="txtContrasenia"
+                class="Login-Contrasenia u-box-sizing text required  u-redondeado--05"
+                placeholder="INGRESE LA CONTRASEÑA"
+                onfocus="if(this.value == 'INGRESE LA CONTRASEÑA') { this.value = ''; }" required>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="u-textRight">
+                <input type="submit" name="btnEntrar" id="btnEntrar"
+                    class="Login-Boton u-boton u-efecto u-textCenter u-redondeado--05 u-inline-block" value="ENTRAR">
             </div>
-        </div>
-    </div>
+            @if (Route::has('password.request'))
+                <a class="btn btn-link text-white" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            @endif
+            <figure class="Notificaciones-Cargado u-textCenter">
+                <img src="images/loading.gif" alt="Cargando..." />
+            </figure>
+        </form>
+
+        <figure class="Login-ImgSSPA slideUp"></figure>
+    </article>
+
 </div>
+@endsection
+
+@section('javascript')
+    <script type="text/javascript" src="{{asset('/js/jamarrom/jquery-ui.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/js/jamarrom/login.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/js/jamarrom/funciones.js')}}"></script>
+    <script type="text/javascript">
+        $(document).ready(function()
+       {
+             $("#txtUsuario").change(function ()
+           {
+               if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0)
+               {
+                   $('input:-webkit-autofill').each(function()
+                   {
+                       var text = $(this).val();
+                       var name = $(this).attr('name');
+                       $(this).after(this.outerHTML).remove();
+                       $('input[name=' + name + ']').val(text);
+                   });
+               }
+           });
+        });
+   </script>
 @endsection
