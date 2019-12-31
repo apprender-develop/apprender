8acb2d50ea97f24ea5f2756532089d4dd67527dc
deltacastro <abel.castro9111@gmail.com>
2019-12-31 18:48:25 +0000

    Se crea layout sin navbar

---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @yield('header-layout')
+    <!-- Scripts -->
+    {{-- <script src="{{ asset('js/jquery-3.4.1.min.map') }}"></script> --}}
+    <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
+    {{-- <script src="{{ asset('js/bootstrap.js.map') }}"></script> --}}
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/bootstrap.css.map') }}"> --}}
+
+    @yield('style')
+</head>
+<body>
+    @yield('content')
+    @yield('javascript')
+    @stack('javascript')
+</body>
+</html>
