9a8c77090ae7ad90053b39b1a64555f7b9a0358c
deltacastro <abel.castro9111@gmail.com>
2019-12-31 18:46:19 +0000

    Se adapta registro al framework

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,91 +1,101 @@
-@extends('layouts.app')
+@extends('layouts.app2')
+
+@section('header-layout')
+    <title>Registro - Apprender</title>
+	<link rel="shortcut icon" href="favicon.png" />
+@endsection
+
+@section('style')
+    <link href="{{'/css/jamarrom/reset.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/fonts.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/utilidades.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/style.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/animations.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{'/css/jamarrom/responsive.css'}}" rel="stylesheet" type="text/css">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="Autentificacion-Ajuste u-imagenFondoCover">
+    <article class="Login" id="login">
+        <h1 class="Login-Logo slideDown"><span>Apprender</span></h1>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+        <form name="form-login" id="form-login" class="Login-Form u-redondeado--1 u-desaparecer" method="POST" action="{{ route('register') }}">
+            <p class="Login-Titulo u-textLeft">
+                Registro
+            </p>
 
-                        <div class="form-group row">
-                            <label for="nombreCompleto" class="col-md-4 col-form-label text-md-right">Nombre Completo</label>
+            @csrf
 
-                            <div class="col-md-6">
-                                <input id="nombreCompleto" type="text" class="form-control @error('nombreCompleto') is-invalid @enderror" name="nombreCompleto" value="{{ old('nombreCompleto') }}" required autocomplete="nombreCompleto" autofocus>
+            <input type="text" id="txtNombre" class="text u-box-sizing required  u-redondeado--05"
+                placeholder="Nombre completo" onfocus="if(this.value == 'Nombre completo') { this.value = ''; }"
+                name="nombreCompleto" value="{{ old('nombreCompleto') ? old('nombreCompleto') : 'Nombre completo' }}" required autocomplete="nombreCompleto" autofocus>
+            @error('nombreCompleto')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-                                @error('nombreCompleto')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <input type="text" id="txtNoFicha" class="text u-box-sizing required  u-redondeado--05"
+                placeholder="No de ficha" onfocus="if(this.value == 'No de ficha') { this.value = ''; }"
+                value="No de ficha" name="pseudoficha" value="{{ old('pseudoficha') ? old('pseudoficha') : 'No de ficha' }}" required autocomplete="pseudoficha">
+            @error('pseudoficha')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <input type="email" id="txtEmail" class="text u-box-sizing required  u-redondeado--05"
+                placeholder="E-mail" onfocus="if(this.value == 'E-mail') { this.value = ''; }" name="email" value="{{ old('email') ? old('email') : 'E-mail' }}" required autocomplete="email">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="pseudoficha" class="col-md-4 col-form-label text-md-right">Pseudoficha</label>
-
-                            <div class="col-md-6">
-                                <input id="pseudoficha" type="text" class="form-control @error('pseudoficha') is-invalid @enderror" name="pseudoficha" value="{{ old('pseudoficha') }}" required autocomplete="pseudoficha">
-
-                                @error('pseudoficha')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div
-
-                        {{-- <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div> --}}
-
-                        {{-- <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div> --}}
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="u-textRight">
+                <input type="button" name="btnEntrar" id="btnEntrar"
+                    class="Login-Boton u-boton u-efecto u-textCenter u-redondeado--05 u-inline-block" value="ENTRAR">
             </div>
-        </div>
-    </div>
+
+            <p class="u-textRight"><a class="Login-TxtTengoCuenta u-inline-block"
+                    href="autentificacion.php"><strong>Tengo cuenta</strong></a></p>
+
+            <p class="u-textRight"><a class="Login-TxtTengoCuenta u-inline-block"
+                    href="autentificacion.php"><strong>Recuperar contraseña</strong></a></p>
+
+            <figure class="Notificaciones-Cargado u-textCenter">
+                <img src="images/loading.gif" alt="Cargando..." />
+            </figure>
+        </form>
+
+        <figure class="Login-ImgSSPA slideUp"></figure>
+    </article>
 </div>
+
+@endsection
+
+@section('javascript')
+    <script type="text/javascript" src="{{asset('/js/jamarrom/jquery-ui.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/js/jamarrom/registro.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/js/jamarrom/funciones.js')}}"></script>
+    <script type="text/javascript">
+        $(document).ready(function()
+        {
+            $("#txtNombre").change(function ()
+            {
+                if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0)
+                {
+                    $('input:-webkit-autofill').each(function()
+                    {
+                        var text = $(this).val();
+                        var name = $(this).attr('name');
+                        $(this).after(this.outerHTML).remove();
+                        $('input[name=' + name + ']').val(text);
+                    });
+                }
+            });
+        });
+    </script>
 @endsection
