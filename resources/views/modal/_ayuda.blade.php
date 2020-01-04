b86bb06e11a364fdf00646eb37af1a0baebd22dc
deltacastro <abel.castro9111@gmail.com>
2020-01-04 18:13:08 +0000

    Se actualiza archivo de ayuda

--- resources/views/modal/_ayuda.blade.php
+++ resources/views/modal/_ayuda.blade.php
@@ -2,28 +2,48 @@
     <div class="ModalProgreso-Center ModalProgreso-Center--Ayuda u-inline-block">
         <span class="Modal-Cerrar u-inline-block">CERRAR <span>x</span></span>
 
-       <h2 class="ModalProgreso-Title u-inline-block">Ayuda</h2>
+        <h2 class="ModalProgreso-Title u-inline-block">Ayuda</h2>
 
-       <div class="ModalGlosario-ContentList ModalGlosario-ContentList--Ayuda u-inline-block u-textLeft">
+        <div class="ModalGlosario-ContentList ModalGlosario-ContentList--Ayuda u-inline-block u-textLeft">
 
-           <p><strong>¿Puedo avanzar más rápido si tengo experiencia previa sobre los temas de la capacitación?</strong></p>
-           <p>Existen dos tipos de experiencia sobre estos temas básicamente. La primera tiene que ver con capacitaciones previas, formales y académicas; la otra se refiere a la formación empírica, en ambos casos recomendamos realizar los ejercicios y las evaluaciones inicialmente para determinar el nivel de conocimiento e identificar las áreas de oportunidad en dónde se deberá hacer un repaso.</p>
-           <p>Idealmente, ya sea que se cuente con poca o mucha experiencia previa, lo recomendable es iniciar la capacitación desde el primer tema.</p>
+            <p><strong>¿Puedo avanzar más rápido si tengo experiencia previa sobre los temas de la
+                    capacitación?</strong></p>
+            <p>Existen dos tipos de experiencia sobre estos temas básicamente. La primera tiene que ver con
+                capacitaciones previas, formales y académicas; la otra se refiere a la formación empírica, en ambos
+                casos recomendamos realizar los ejercicios y las evaluaciones inicialmente para determinar el nivel de
+                conocimiento e identificar las áreas de oportunidad en dónde se deberá hacer un repaso.</p>
+            <p>Idealmente, ya sea que se cuente con poca o mucha experiencia previa, lo recomendable es iniciar la
+                capacitación desde el primer tema.</p>
 
-           <p><strong>¿Cómo puedo inscribirme a nuevos cursos de capacitación?</strong></p>
-           <p>Los cursos de capacitación se encuentran ligados, comparten bases de conocimiento y elementos que nos permiten entender mejor la información dispuesta en cada uno de ellos, para que un nuevo curso sea liberado es necesario haber concluido el anterior con un porcentaje de aprovechamiento igual o superior al promedio que el capacitador establezca.</p>
+            <p><strong>¿Cómo puedo inscribirme a nuevos cursos de capacitación?</strong></p>
+            <p>Los cursos de capacitación se encuentran ligados, comparten bases de conocimiento y elementos que nos
+                permiten entender mejor la información dispuesta en cada uno de ellos, para que un nuevo curso sea
+                liberado es necesario haber concluido el anterior con un porcentaje de aprovechamiento igual o superior
+                al promedio que el capacitador establezca.</p>
 
-           <p><strong>¿Cómo funcionan las lecciones y las unidades?</strong></p>
-           <p>La capacitación se encuentra dividida en lecciones, las cuales son la unidad mínima de capacitación, un conjunto de lecciones forman una unidad; dependiendo de los temas las unidades tendrán mayor o menor número de lecciones. Al terminar una lección obtienes un porcentaje de avance, algunas lecciones además poseen ejercicios y autoevaluaciones, de tal manera que puedas medir adecuadamente tus conocimientos y avances.</p>
-           <p>Al terminar un grupo de lecciones que forman la unidad, se presenta un examen general de la unidad, el cual identifica los conceptos básicos de cada lección y los evalúa, accedes a la siguiente unidad cuando acumulas los puntos necesarios para probar la unidad anterior. No sólo obtienes puntos al realizar las evaluaciones, los puntos se van sumando de acuerdo a las lecciones que has completado y los ejercicios realizados.</p>
+            <p><strong>¿Cómo funcionan las lecciones y las unidades?</strong></p>
+            <p>La capacitación se encuentra dividida en lecciones, las cuales son la unidad mínima de capacitación, un
+                conjunto de lecciones forman una unidad; dependiendo de los temas las unidades tendrán mayor o menor
+                número de lecciones. Al terminar una lección obtienes un porcentaje de avance, algunas lecciones además
+                poseen ejercicios y autoevaluaciones, de tal manera que puedas medir adecuadamente tus conocimientos y
+                avances.</p>
+            <p>Al terminar un grupo de lecciones que forman la unidad, se presenta un examen general de la unidad, el
+                cual identifica los conceptos básicos de cada lección y los evalúa, accedes a la siguiente unidad cuando
+                acumulas los puntos necesarios para probar la unidad anterior. No sólo obtienes puntos al realizar las
+                evaluaciones, los puntos se van sumando de acuerdo a las lecciones que has completado y los ejercicios
+                realizados.</p>
 
-           <p><strong>¿Aún tienes dudas? Ésta es una herramienta de capacitación diseñada para ser utilizada de forma inmediata y fácil para los usuarios, si tienes alguna duda puedes solicitar mayor información desde nuestro chat online o escribiendo un correo electrónico desde los siguientes enlaces.</strong></p>
+            <p><strong>¿Aún tienes dudas? Ésta es una herramienta de capacitación diseñada para ser utilizada de forma
+                    inmediata y fácil para los usuarios, si tienes alguna duda puedes solicitar mayor información desde
+                    nuestro chat online o escribiendo un correo electrónico desde los siguientes enlaces.</strong></p>
 
-           <div class="u-textCenter">
-               <a class="ModalAyuda-Btn u-inline-block u-boton u-redondeado--05" href="javascript:void($zopim.livechat.say('Hola, En que puedo ayudarte!'))">Chat Online</a>
-               <a class="ModalAyuda-Btn u-inline-block u-boton u-redondeado--05" href="mailto:teayudo@cursoonline.com">Enviar e-mail</a>
-           </div>
+            <div class="u-textCenter">
+                <a class="ModalAyuda-Btn u-inline-block u-boton u-redondeado--05"
+                    href="javascript:void($zopim.livechat.say('Hola, En que puedo ayudarte!'))">Chat Online</a>
+                <a class="ModalAyuda-Btn u-inline-block u-boton u-redondeado--05"
+                    href="mailto:teayudo@cursoonline.com">Enviar e-mail</a>
+            </div>
 
-       </div>
-   </div>
+        </div>
+    </div>
 </div>
