8ef2061bddff5f2eac885ece325da8911972d9c6
deltacastro <abel.castro9111@gmail.com>
2020-01-05 08:27:12 +0000

    Se agregan seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(AccionesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(AdminTableSeeder::class);
     }
 }
