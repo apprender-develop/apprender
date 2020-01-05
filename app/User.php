d19a478ef0629824772325f62aea8c19ba52bc5c
deltacastro <abel.castro9111@gmail.com>
2020-01-05 08:26:38 +0000

    Se agrega metodo historial

--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function historial()
+    {
+        return $this->hasMany(Historial_Usuario::class, 'user_id');
+    }
 }
