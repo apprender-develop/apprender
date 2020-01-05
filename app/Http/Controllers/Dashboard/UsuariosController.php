dbdafa055cbbde2e76d390f082a1a6073dae24c6
deltacastro <abel.castro9111@gmail.com>
2020-01-05 08:28:24 +0000

    Se agrega filtrado de usuario y constructor para el modelo usuario

--- app/Http/Controllers/Dashboard/UsuariosController.php
+++ app/Http/Controllers/Dashboard/UsuariosController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Dashboard;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UsuariosController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->mUser = new User;
+    }
+
     public function index()
     {
-        return view('dashboard.usuarios.index');
+        $users = $this->mUser->whereHas("roles", function($q){ $q->where("name", "cliente"); })->get();
+        return view('dashboard.usuarios.index', compact('users'));
     }
 }
