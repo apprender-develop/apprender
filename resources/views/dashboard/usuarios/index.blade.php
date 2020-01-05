c12b930c99f4cdd79093237d3e188336b2b2be3c
deltacastro <abel.castro9111@gmail.com>
2020-01-05 07:31:04 +0000

    Se agregan las vistas de usuarios

---
+++ resources/views/dashboard/usuarios/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.delta')
+
+@section('style')
+
+@endsection
+
+@section('content')
+    <div class="col-12">
+        aqui va algo
+    </div>
+@endsection
+
+@section('javascript')
+
+@endsection
