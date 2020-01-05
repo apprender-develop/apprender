7fc9815d22d552eb32df437e656461143f033436
deltacastro <abel.castro9111@gmail.com>
2020-01-05 08:28:03 +0000

    Se agrega un card para la tabla

--- resources/views/dashboard/usuarios/index.blade.php
+++ resources/views/dashboard/usuarios/index.blade.php
@@ -5,8 +5,13 @@
 @endsection
 
 @section('content')
-    <div class="col-12">
-        aqui va algo
+    <div class="card">
+        <div class="card-header">
+            Lista de usuarios que se registraron
+        </div>
+        <div class="card-body">
+            @include('dashboard.usuarios._list')
+        </div>
     </div>
 @endsection
 
