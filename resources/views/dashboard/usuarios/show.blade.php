2a3cf1e14fc83ce37b5c104c218f6b8b30b00661
deltacastro <abel.castro9111@gmail.com>
2020-01-06 20:02:16 +0000

    Actualizacion de fechas

--- resources/views/dashboard/usuarios/show.blade.php
+++ resources/views/dashboard/usuarios/show.blade.php
@@ -12,12 +12,12 @@
         <div class="card-body">
             <ul class="nav nav-pills flex-column flex-sm-row mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item">
-                    <a class="flex-sm-fill text-center nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-perfil" role="tab"
-                        aria-controls="pills-home" aria-selected="true">Perfil</a>
+                    <a class="flex-sm-fill text-center nav-link active" id="pills-profile-tab" data-toggle="pill" href="#pills-historial" role="tab"
+                        aria-controls="pills-profile" aria-selected="false">Historial de navegacion</a>
                 </li>
                 <li class="nav-item">
-                    <a class="flex-sm-fill text-center nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-historial" role="tab"
-                        aria-controls="pills-profile" aria-selected="false">Historial de navegacion</a>
+                    <a class="flex-sm-fill text-center nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-perfil" role="tab"
+                        aria-controls="pills-home" aria-selected="true">Perfil</a>
                 </li>
                 <li class="nav-item">
                     <a class="flex-sm-fill text-center nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab"
@@ -25,11 +25,11 @@
                 </li>
             </ul>
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-perfil" role="tabpanel" aria-labelledby="pills-home-tab">
-                    @include('dashboard.usuarios.detalles._perfil')
-                </div>
-                <div class="tab-pane fade" id="pills-historial" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <div class="tab-pane fade show active" id="pills-historial" role="tabpanel" aria-labelledby="pills-profile-tab">
                     @include('dashboard.usuarios.detalles._historial')
+                </div>
+                <div class="tab-pane fade" id="pills-perfil" role="tabpanel" aria-labelledby="pills-home-tab">
+                    @include('dashboard.usuarios.detalles._perfil')
                 </div>
                 <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
             </div>
