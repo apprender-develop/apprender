6fcf700aaeb644cf84dd87a83fff41367164e807
deltacastro <abel.castro9111@gmail.com>
2020-01-06 20:01:47 +0000

    Se agrega constructor con la clase encuesta y se invoca el metodo apna en la funcion index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Encuesta;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->mEncuesta = new Encuesta;
+    }
+
     public function index()
     {
-        return view('dashboard.index');
+        $apna = $this->mEncuesta->apna();
+        $apna = json_encode($apna);
+        return view('dashboard.index', compact('apna'));
     }
 }
