b7f64eac1fea081a4e7a62e39e1384b5e8f38dc5
deltacastro <abel.castro9111@gmail.com>
2020-01-06 17:24:49 +0000

    Se corrigen unidades

--- resources/views/cursos/1/unidad4/2.blade.php
+++ resources/views/cursos/1/unidad4/2.blade.php
@@ -6,7 +6,7 @@
 </script>
 
 <div class="Unidad-ContentInfo Unidad-ContentInfo--Introduccion u-inline-block u-textJustify">
-	<h4 class="Unidad-TitleContentInfo u-textLeft">2.2 INTRODUCCIÓN</h4>
+	<h4 class="Unidad-TitleContentInfo u-textLeft">4.2 INTRODUCCIÓN</h4>
 	<p>En Petróleos Mexicanos la Seguridad Industrial y Protección Ambiental estaba desvinculada de la organización, con esfuerzos aislados enfocados a resolver acciones correctivas a corto plazo más que a la prevención; sin el involucramiento de la administración y el compromiso de la alta Dirección.</p>
 	<p>Hoy en día, la difusión y el conocimiento de la política de seguridad, salud y Protección Ambiental, son una herramienta administrativa para alcanzar la meta CERO accidentes como estrategia de PEMEX.</p>
 	<p>Son elementos básicos en la administración de riesgos para lograr ventajas competitivas y de sustentabilidad petrolera a nivel internacional que el capacitando debe conocer.</p>
