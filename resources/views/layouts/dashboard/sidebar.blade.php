9b89f0566f7df5f15a7c4bebe7759e54df29cae8
deltacastro <abel.castro9111@gmail.com>
2020-01-06 21:11:33 +0000

    Se agrega logo

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -1,5 +1,8 @@
 <div class="nav-side-menu sidebar">
-    <div class="brand">Brand Logo</div>
+    <div class="brand bg-info">
+        <img src="{{asset('/images/logoPlataformaSolo.png')}}" class="img-fluid" alt="">
+        {{-- Apprender --}}
+    </div>
     <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
 
     <div class="menu-list">
