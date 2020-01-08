ea833a4ed9012dc8faa43b424c32f30ee5649f18
deltacastro <abel.castro9111@gmail.com>
2020-01-08 23:46:35 +0000

    Se crea vista

---
+++ resources/views/dashboard/datos/_caliPlataforma.blade.php
@@ -0,0 +1,14 @@
+<div class="card shadow-lg mb-5 rounded border-0">
+    <h5 class="card-header bg-info text-light">Calificación General Plataforma</h5>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-2">
+                <i class="fab fa-4x fa-laravel"></i>
+            </div>
+            <div class="col-10">
+
+                <p class="card-text text-right" style="font-size: 42px;">{{$caliTotal}}</p>
+            </div>
+        </div>
+    </div>
+</div>
